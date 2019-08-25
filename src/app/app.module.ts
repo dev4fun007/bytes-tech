2a60c690b35abb12cdb336ce6cf8260321dfa554
dev4fun007 <dev4fun007@gmail.com>
2019-08-25 23:54:30 +0530

    adding missed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-
+import { StickyHeaderModule, NavbarModule } from 'angular-bootstrap-md';
+import { NavToolbarComponent } from './components/nav-toolbar/nav-toolbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { FooterCommonComponent } from './components/footer-common/footer-common.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavToolbarComponent,
+    FooterCommonComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    StickyHeaderModule,
+    NavbarModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
