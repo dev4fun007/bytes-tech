cef773186d06034da18e88649869db9d0fc38c08
dev4fun007 <dev4fun007@gmail.com>
2019-08-25 23:53:42 +0530

    added nav header and footer

---
+++ src/app/components/footer-common/footer-common.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-common',
+  templateUrl: './footer-common.component.html',
+  styleUrls: ['./footer-common.component.scss']
+})
+export class FooterCommonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
